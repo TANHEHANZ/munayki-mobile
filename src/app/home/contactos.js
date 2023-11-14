c142e4764c9c4a2b82bdcb439109bc5c19830ae7
noeliamorato <moratocaeronoelia123@gmail.com>
2023-11-14 12:37:58 -0400

    dataContact

--- src/app/home/contactos.js
+++ src/app/home/contactos.js
@@ -1,14 +1,30 @@
 import { View, Text } from "react-native";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { contactStyle, loginstyle } from "../../styles/style";
 import { colors } from "../../styles/CompStyle";
 import InputsContact from "../../components/global/inputsContact";
-import { peticionPost } from "../../utilitis/postRequest";
+import { peticionGet } from "../../utilitis/getRequest";
 
 const Contactos = () => {
   const [mostrar, setMostrar] = useState(false);
-  
+  const [data, setData] = useState("");
+const user =1;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await peticionGet("user/"+user+"/contacts");
+        setData(result);
+      } catch (error) {
+        console.error("Error al obtener datos:", error);
+      }
+    };
+
+    fetchData();
+  }, []); 
+
+  console.log(data)
+ 
   return (
     <View
       style={{
@@ -17,7 +33,7 @@ const Contactos = () => {
         alignItems: "center",
       }}
     >
-      <Text style={{ margin: 30 }}>Contactos Registrados : 0</Text>
+      <Text style={{ margin: 30 }}>Contactos Registrados :{data.length}</Text>
 
      <InputsContact mostrar={mostrar}/>
 

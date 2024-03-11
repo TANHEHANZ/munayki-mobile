44e65730552c0377d477c9b3386ce6aed9ec3c8a
TANHEHANZ <hantach10@email.com>
2024-03-11 10:57:02 -0400

    configuracion del servr

--- src/utilitis/http.js
+++ src/utilitis/http.js
@@ -1,11 +1,3 @@
-// export const http = "https://munayki-serve.vercel.app/"
-// export const http = "https://servermunaikidata.vercel.app/"
 
-// -----------------------server v2 ----------
-
-// export const http = "https://munaykiv-2.vercel.app/"
-
-
-// export const http = "http://192.168.0.27:3000/";
 export const http = "https://munaykiapi.cochabamba.bo";
-// VITE_BACKEND_URL="https://servermunaikidata.vercel.app"
+
